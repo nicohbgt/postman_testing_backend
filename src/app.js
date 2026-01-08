e633ea79c2385aceb7a77c7557f073571f1e9caa
nicohbgt <nicohbgt512@gmail.com>
2026-01-08 20:34:07 +0700

    coding 1

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const middleware = require('./middleware/auth.middleware');
 
 app.use(express.json());
+app.use('/auth', require('./router/auth.route'));
 
 app.get("/", (req, res) => {
     res.send("Backend is working!");
-})
+});
+
+app.get("/dashboard", authMiddleware, (req, res) => {
+    res.send("Welcome to the dashboard!");
+});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,2 +1,11 @@
 const users = [];
 module.exports = users;
+
+const express = require('express');
+const router = express.router();
+const authContoller = require('../controllers/auth.controller');
+
+router.post("register", authController.register);
+router.post("login", authController.login);
+
+module.exports = router;
