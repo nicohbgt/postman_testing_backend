8251b18fbd33d422526b96ec896ba0b132c941d0
nicohbgt <nicohbgt512@gmail.com>
2026-01-08 18:59:35 +0700

    dotenv created

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -7,6 +8,6 @@ app.get("/", (req, res) => {
     res.send("Backend is working!");
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
